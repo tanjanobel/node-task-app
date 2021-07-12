6c7fb82d80b284a2a783071be19c5aa74b8ba145
Tanja Nobel <tanja@nobel.swiss>
2021-07-12 14:47:15 +0200

    Add REST API route for creating new user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
    useNewUrlParser: true,
@@ -16,14 +15,4 @@ const Task = mongoose.model('Task', {
        type: Boolean,
         default: false
     }
-});
-
-const task = new Task ({
-    description: '   Eat lunch',
-});
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error);
 });
