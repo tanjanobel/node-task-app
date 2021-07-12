09990dd10df1ee7632289845b9e63e4ee119a4a9
Tanja Nobel <tanja@nobel.swiss>
2021-07-12 11:38:21 +0200

    Create mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,3 +4,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
    useNewUrlParser: true,
    useCreateIndex: true
 });
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+       type: Boolean
+    }
+});
+
+const task = new Task ({
+    description: 'Learn the Mongoose Library',
+    completed: false
+});
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error);
+});
