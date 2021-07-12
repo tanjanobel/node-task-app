d121394328ca350268d9dc8cdb46a7c04ef14e04
Tanja Nobel <tanja@nobel.swiss>
2021-07-12 09:03:36 +0200

    Add tasks to db

--- mongodb.js
+++ mongodb.js
@@ -13,8 +13,22 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Tanja',
-        age: 32
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        }, {
+            description: 'Renew inspection',
+            completed: false
+        }, {
+            description: 'Pot plants',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!');
+        }
+
+        console.log(result.ops);
     });
 });
