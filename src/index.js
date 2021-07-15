35d3283ed51bb24ef81d366f1983b925bdee9086
Tanja Nobel <tanja@nobel.swiss>
2021-07-15 11:21:51 +0200

    Add route for getting tasks by id

--- src/index.js
+++ src/index.js
@@ -50,6 +50,27 @@ app.post('/tasks', (req, res) => {
     });
 });
 
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
     console.log('Sever is up on port ' + port);
 });
