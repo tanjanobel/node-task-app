267b8570ba01de83f41e18ea6aaa587074dba937
Tanja Nobel <tanja@nobel.swiss>
2021-07-16 13:41:03 +0200

    Add route for deleting task

--- src/index.js
+++ src/index.js
@@ -138,6 +138,20 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 });
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
 app.listen(port, () => {
     console.log('Sever is up on port ' + port);
 });
